cfda94538c9573ef696fef9f084df6b6139627e2
kamauvick <kamauvick@outlook.com>
2020-07-25 17:25:54 +0300

    Made a fix to the Units Url's.

--- attend_class_app/views.py
+++ attend_class_app/views.py
@@ -1,36 +1,55 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, DetailView, View)
 from .models import Units
+from django import template
 
-context = {
-            'units': Units.objects.all()
-        }
+register = template.Library()
+
+
+arr = []
+context =  Units.objects.all().values()
+
+for i in context:
+    arr.append(i)
+
+print(arr)
 
 
 def home(request):
-
-    context = {
-        'units': Units.objects.all()
-    }
     return render(request, 'attend_class_app/home.html', context)
 
+# class UnitListView(ListView):
+#     print(context)
+#     model = Units
+#     template_name = 'attend_class_app/home.html'
+#     context_object_name = 'units'
+
+def UnitListView(request):
+    units =  Units.objects.all()
+    return render(request, "attend_class_app/home.html", {"units": units})
 
 
-class UnitListView(ListView):
-    model = Units
-    template_name = 'attend_class_app/home.html'
-    context_object_name = 'units'
 
-#class UnitDetailView(DetailView):
-    #model = Units
-    #template_name = 'attend_class_app/unit_detail.html'
-    #context_object_name = 'units'
+# def UnitDetailView(request):
+#     print(context)
+#     units =  Units.objects.all()
+
+#     return render(request, "attend_class_app/home.html", {"units": units})
+
+
+
+class UnitDetailView(View):
+    def get(self, request, *args, **kwargs):
+        # id  = request.GET["id"]
+        return render(request, 'attend_class_app/unit_detail.html', {'units' :arr})
+
+ 
     
 
-def UnitDetailView(request):
-    #if request.method == 'POST':
-        #print(context)
-    return render (request, 'attend_class_app/unit_detail.html' , { 'context' : context })
+# def UnitDetailView(request):
+#     if request.method == 'POST':
+#         print(context)
+#     return render (request, 'attend_class_app/unit_detail.html' , { 'context' : context })
 
 def about(request):
     return render(request, 'attend_class_app/about.html', {'title':'About'})  
